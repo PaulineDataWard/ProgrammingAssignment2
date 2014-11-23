a0ba69c5b54caae7ab3dddfc3a0998c8be4dd0ad
PaulineDataWard <pauline.ward@ed.ac.uk>
2014-11-23 18:57:47 +0000

    two functions which pass a matrix between them for cacheing

--- cachematrix.R
+++ cachematrix.R
@@ -1,20 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions allow the caching of an inverse matrix and its retrieval
 
-## Write a short comment describing this function
+
+## This function creates a special matrix object and caches it.
 
 makeCacheMatrix <- function(x = matrix()) {
-  # This function creates a special "matrix" object that can cache its inverse.
   
+  # If nothing has been cached as yet...
+  if (cachedMatrix == NULL) {
+    # Cache the matrix
+    cachedMatrix <- x   
+  }
+    
+  cachedMatrix
 }
 
 
-## Write a short comment describing this function
+## This function checks whether a cached copy of an inverteds matrix exists,
+## if so, fetches it, if not, inverts it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   # This function computes the inverse of the special "matrix" returned by makeCacheMatrix().
   # If the inverse has already been calculated (and the matrix has not changed), 
   # then the cachesolve will retrieve the inverse from the cache.
-  solve(x)
+  
+  # If no matrix has been inverted and cached as yet...
+  if (makeCachedMatrix(NULL) == NULL){
+    # Invert the matrix and send it to be cached
+    theAnswer <- solve(x)
+    makeCacheMatrix(theAnswer)
+    lastMatrixCached <- x
+  }
+  
+  else {
+    # Compare matrix received to the one that has been cached
+    sameMatrixFlag <- NULL
+    sameMatrixTest <- as.logical(lastMatrixCached == x)
+    for (i in sameMatrixTest){
+      if (!sameMatrixTest[i]){
+        sameMatrixFlag <= FALSE
+      }
+    }
+    
+    if (!sameMatrixTest){
+      # Invert the matrix and send it to be cached
+      theAnswer <- solve(x)
+      makeCacheMatrix(theAnswer)
+    }
+    
+  }
+    
+  theAnswer
 }
