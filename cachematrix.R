94734cea163bc9dec14352bf9a4b5f552804cb77
PaulineDataWard <pauline.ward@ed.ac.uk>
2014-11-23 15:46:14 +0000

    Making sure I'm able to push my assignment to where I need to.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,8 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # This function creates a special "matrix" object that can cache its inverse.
+  
 }
 
 
@@ -12,4 +13,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  # This function computes the inverse of the special "matrix" returned by makeCacheMatrix().
+  # If the inverse has already been calculated (and the matrix has not changed), 
+  # then the cachesolve will retrieve the inverse from the cache.
+  solve(x)
 }
